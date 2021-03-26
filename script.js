1bd9d1ee4b0bee06ece31020c3f255b3acca2b26
Ian F <ian.ferg36@gmail.com>
2021-03-26 14:10:22 -0600

    tweaked time blocks on html, made functions, set current time

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-var textArea = $("textarea");
-var saveButton = $("saveBtn");
-var currentDay = $("currentDay");
+var currentDay = $("#currentDay");
+function currentDayofDate() {
+  currentDay.text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  console.log(currentDay);
+}
+function timeBlocks() {
+  $(".time-block").each(function () {
+    console.log($(this).attr("id").split("-")[1]);
+  });
+}
+function saveButton() {}
+timeBlocks();
+currentDayofDate();
