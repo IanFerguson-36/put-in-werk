7b161385fab69782a30819082c8c9c06091633cc
Ian F <ian.ferg36@gmail.com>
2021-03-27 13:51:57 -0600

    readme completed, got as far on the jquery as I could

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # put-in-werk
+
+# Decription
+
+Day planner, that keeps current time, could not get over the wall of making it light up or using local storage.
+![Portfolio  Site](./assets/ianferguson-36.github.io_put-in-werk_.png)
+
+# Deployed Site
+
+heres the site
+[Deployed Site](https://ianferguson-36.github.io/put-in-werk/)
--- script.js
+++ script.js
@@ -6,19 +6,18 @@ function currentDayofDate() {
 }
 function timeBlocks() {
   var now = moment().format("h:mm:ss a");
-  $("time-block").each(function(){
-     console.log($(this).attr('id'))
-if (timeLoop < now) {
-    $("description").addClass("past");
-  } else if (timeLoop > now) {
-    $("description").addClass("future");
-  } else {
-    $("description").addClass("present");
-  }
-counter++;
-}
+  $("time-block").each(function () {
+    console.log($(this).attr("id"));
+    if (timeLoop < now) {
+      $("description").addClass("past");
+    } else if (timeLoop > now) {
+      $("description").addClass("future");
+    } else {
+      $("description").addClass("present");
+    }
+    counter++;
+  });
 }
 
-function saveButton() {}
 timeBlocks();
 currentDayofDate();
